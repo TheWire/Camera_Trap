9a1b8c5c45fd91f1c4c010700d0cee254db3cc9f
TheWire <TheWire@users.noreply.github.com>
2025-06-11 15:43:20 +0100

    image recognition test

--- camera_trap.py
+++ camera_trap.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, render_template
 from picamera2 import Picamera2
 import time
 import cv2
+from ultralytics import YOLO
 
 app = Flask(__name__)
 
@@ -10,11 +11,33 @@ camera_config = picam2.create_video_configuration(main={"size": (640, 480)})  # 
 picam2.configure(camera_config)
 picam2.start()
 
+model = YOLO("yolo11n.pt")
+names = model.names
+threshold = .40
+
 def generate_frames():
     """Generator function to capture and yield JPEG frames for MJPEG streaming."""
     while True:
         # Capture a frame as JPEG
         frame = picam2.capture_array()
+        results = model(frame)
+        annotated = results[0].plot()
+        boxes = results[0].boxes.xyxy.cpu().tolist()
+        clss = results[0].boxes.cls.cpu().tolist()
+        confs = results[0].boxes.conf.cpu().tolist()
+        if boxes is not None:
+            # if set(clss) == previous_classes:
+            #     print("Scene is unchanged, not saving images.")
+            #     return set(clss)
+
+            for box, cls, conf in zip(boxes, clss, confs):
+                if conf < threshold: # Is confidence under threshold?
+                    continue
+                cv2.rectangle(frame,
+                              (int(box[0]), int(box[1])),
+                              (int(box[2]), int(box[3])),
+                              (255, 0, 0), 2)
+
         # Convert to JPEG format
         _, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
