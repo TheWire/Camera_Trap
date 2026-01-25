4fa83d062fc97cff97fe8c4901aea2a4d7832932
TheWire <andrew.ab@gmail.com>
2026-01-25 15:05:43 +0000

    delete duplicate def LDR_value

--- camera_trap.py
+++ camera_trap.py
@@ -138,31 +138,6 @@ def video():
 
     return response
 
-def LDR_value(pin, charge_time_limit=0.005):
-    
-    # Take the pin LOW to discharge the capacitor
-    ldr = OutputDevice(pin=pin, active_high=True, 
-                       initial_value=False)
-    sleep(0.1)
-
-    # Configure the pin as an input
-    ldr.close()
-    ldr = InputDevice(pin=pin, pull_up=None,
-                      active_state=True)
-
-    # Wait for the capacitor to recharge, but only up to
-    # the charge time limit
-    lit = 0
-    start = time()
-    while True:
-        if time() - start >  charge_time_limit:
-            break
-        if ldr.is_active:
-            lit += 1
-
-    ldr.close()
-    return lit
-
 @app.route('/api/light', methods=["post"])
 def light():
     content = request.get_json()
